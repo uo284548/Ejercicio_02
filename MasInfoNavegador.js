7e478ed3ef9ca6feafcd802d86cfa0929b9c37af
uo284548 <113984522+uo284548@users.noreply.github.com>
2022-11-21 17:40:31 +0100

    Add files via upload

---
+++ MasInfoNavegador.js
@@ -0,0 +1,5 @@
+document.write("Versión: "+infoNavegador.version+"</p>");
+document.write("<p>Plataforma: "+infoNavegador.plataforma+"</p>");
+document.write("<p>Vendedor: "+infoNavegador.vendedor+"</p>");
+document.write("<p>Agente: "+infoNavegador.agente+"</p>");
+document.write("<p>Java Activo: "+infoNavegador.javaActivo);
